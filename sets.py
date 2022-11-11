2ef47b36702b024feeac846f4d65efb6bd2b41bc
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2022-11-11 21:08:25 +0530

    tuple sets

---
+++ sets.py
@@ -0,0 +1,7 @@
+fruits = {'mango','apple','orange','grapes'}
+print(fruits)
+# fruits[0]='banana'
+fruits.add('pineapple')
+print(fruits)
+fruits.remove('mango')
+print(fruits)
---
+++ tuple.py
@@ -0,0 +1,6 @@
+book = ('the greates',250,555,'bean',250)
+print(book)
+temp=book.count(250)
+print(temp)
+temp=book.index(250)
+print(temp)
